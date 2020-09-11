4e9f1ca9cd3838e59355fc26ee6077d9aa332942
Richard Saavedra <richardcmg7@gmail.com>
2020-09-10 19:58:32 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,5 @@
-
 # run_analysis.R
-
 library(reshape2)
-
 DataDir <- "./Data"
 DataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 DataFilename <- "Data.zip"
@@ -19,20 +16,17 @@ if (!file.exists(dataDir)) {
     unzip(zipfile = DataDFn, exdir = dataDir)
 }
 
-# Merge {train, test} data set
-# refer: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
-# train data
-
+#train
 x_train <- read.table(paste(sep = "", "./UCI HAR Dataset/train/X_train.txt"))
 y_train <- read.table(paste(sep = "", "./UCI HAR Dataset/train/Y_train.txt"))
 s_train <- read.table(paste(sep = "", "./UCI HAR Dataset/train/subject_train.txt"))
 
-# test data
+# data test
 x_test <- read.table(paste(sep = "", "./UCI HAR Dataset/test/X_test.txt"))
 y_test <- read.table(paste(sep = "", "./UCI HAR Dataset/test/Y_test.txt"))
 s_test <- read.table(paste(sep = "", "./UCI HAR Dataset/test/subject_test.txt"))
 
-# merge {train, test} data
+# merge data train and data test
 x_data <- rbind(x_train, x_test)
 y_data <- rbind(y_train, y_test)
 s_data <- rbind(s_train, s_test)
@@ -45,7 +39,7 @@ feature <- read.table(paste(sep = "", "./UCI HAR Dataset/features.txt"))
 a_label <- read.table(paste(sep = "", "./UCI HAR Dataset/activity_labels.txt"))
 a_label[,2] <- as.character(a_label[,2])
 
-# extract feature cols & names named 'mean, std'
+# Feature cols & names named 'mean, std'
 selectedCols <- grep("-(mean|std).*", as.character(feature[,2]))
 selectedColNames <- feature[selectedCols, 2]
 selectedColNames <- gsub("-mean", "Mean", selectedColNames)
@@ -53,11 +47,10 @@ selectedColNames <- gsub("-std", "Std", selectedColNames)
 selectedColNames <- gsub("[-()]", "", selectedColNames)
 
 
-# extract data by cols & using descriptive name
+# Extract by columnss & using descriptive name
 x_data <- x_data[selectedCols]
 allData <- cbind(s_data, y_data, x_data)
 colnames(allData) <- c("Subject", "Activity", selectedColNames)
-
 allData$Activity <- factor(allData$Activity, levels = a_label[,1], labels = a_label[,2])
 allData$Subject <- as.factor(allData$Subject)
 
@@ -65,5 +58,4 @@ allData$Subject <- as.factor(allData$Subject)
 # Generate tidy data set
 meltedData <- melt(allData, id = c("Subject", "Activity"))
 tidyData <- dcast(meltedData, Subject + Activity ~ variable, mean)
-
-write.table(tidyData, "./tidy_dataset.txt", row.names = FALSE, quote = FALSE)
+write.table(tidyData, "./tidy_dataset.txt", quote = FALSE, row.names = TRUE)
